b81f8fa9b68d1866ef923f5652ca00a4819c1baa
Sergey-sply <sergeysedov12@gmail.com>
2023-09-01 23:00:08 +0300

    'create-rep'

---
+++ main/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from .views import (index, other_page, profile, user_activate, BBLoginView, BBLogoutView, ProfileEditView,
+                    PasswordEditView, RegisterView, RegisterDoneView, ProfileDeleteView)
+
+app_name = 'main'
+
+urlpatterns = [
+    path('accounts/activate/<str:sign>/', user_activate, name='activate'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/password/edit/', PasswordEditView.as_view(), name='password_edit'),
+    path('accounts/profile/delete/', ProfileDeleteView.as_view(), name='profile_delete'),
+    path('accounts/profile/edit/', ProfileEditView.as_view(), name='profile_edit'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('<str:page>/', other_page, name='other'),
+    path('', index, name='index'),
+]
