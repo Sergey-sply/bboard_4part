b9b419c06d0c69fb972ae0125ea02f57c461a9af
Sergey-sply <sergeysedov12@gmail.com>
2023-09-10 21:10:55 +0300

    'add-.env'

--- bboard/settings.py
+++ bboard/settings.py
@@ -12,20 +12,28 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import environ
 import django.core.mail.backends.console
+
+env = environ.Env(
+    DEBUG=(bool),
+    SECRET_KEY=(str),
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-su9m9syywj#gv2$@u99ow%$nkt2x2(joot5$9fmu4lljt!@8@4'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
